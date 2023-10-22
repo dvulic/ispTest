3375270e08af8341e7dec283569628551eb7648d
dvulic <dvulic9323rn@raf.rs>
2023-10-22 14:47:33 +0200

    Added notification after questions are loaded

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,9 @@ import model.utility.FileUtils;
 public class Main {
     public static void main(String[] args) {
         FileUtils.UcitajPitanja("src/pitanja.txt");
+        System.out.println("Questions loaded");
         Application.launch(App.class, args);
+
+
     }
 }
